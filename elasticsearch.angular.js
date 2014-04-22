c5d10101fbedc602ce323dcd1e8f093b7341c5bf
Spencer Alger <spencer@spenceralger.com>
2014-04-22 13:50:57 -0700

    version 2.1.4

--- elasticsearch.angular.js
+++ elasticsearch.angular.js
@@ -1,4 +1,4 @@
-/*! elasticsearch - v2.1.3 - 2014-04-15
+/*! elasticsearch - v2.1.4 - 2014-04-22
  * http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/index.html
  * Copyright (c) 2014 Elasticsearch BV; Licensed Apache 2.0 */
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -19631,14 +19631,19 @@ api.cat.prototype.threadPool = ca({
  * @param {String, String[], Boolean} params.scrollId - A comma-separated list of scroll IDs to clear
  */
 api.clearScroll = ca({
-  url: {
-    fmt: '/_search/scroll/<%=scrollId%>',
-    req: {
-      scrollId: {
-        type: 'list'
+  urls: [
+    {
+      fmt: '/_search/scroll/<%=scrollId%>',
+      req: {
+        scrollId: {
+          type: 'list'
+        }
       }
+    },
+    {
+      fmt: '/_search/scroll'
     }
-  },
+  ],
   method: 'DELETE'
 });
 
@@ -24822,14 +24827,19 @@ api.cat.prototype.threadPool = ca({
  * @param {String, String[], Boolean} params.scrollId - A comma-separated list of scroll IDs to clear
  */
 api.clearScroll = ca({
-  url: {
-    fmt: '/_search/scroll/<%=scrollId%>',
-    req: {
-      scrollId: {
-        type: 'list'
+  urls: [
+    {
+      fmt: '/_search/scroll/<%=scrollId%>',
+      req: {
+        scrollId: {
+          type: 'list'
+        }
       }
+    },
+    {
+      fmt: '/_search/scroll'
     }
-  },
+  ],
   method: 'DELETE'
 });
 
--- elasticsearch.js
+++ elasticsearch.js
@@ -1,4 +1,4 @@
-/*! elasticsearch - v2.1.3 - 2014-04-15
+/*! elasticsearch - v2.1.4 - 2014-04-22
  * http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/index.html
  * Copyright (c) 2014 Elasticsearch BV; Licensed Apache 2.0 */
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.elasticsearch=e():"undefined"!=typeof global?global.elasticsearch=e():"undefined"!=typeof self&&(self.elasticsearch=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -24630,14 +24630,19 @@ api.cat.prototype.threadPool = ca({
  * @param {String, String[], Boolean} params.scrollId - A comma-separated list of scroll IDs to clear
  */
 api.clearScroll = ca({
-  url: {
-    fmt: '/_search/scroll/<%=scrollId%>',
-    req: {
-      scrollId: {
-        type: 'list'
+  urls: [
+    {
+      fmt: '/_search/scroll/<%=scrollId%>',
+      req: {
+        scrollId: {
+          type: 'list'
+        }
       }
+    },
+    {
+      fmt: '/_search/scroll'
     }
-  },
+  ],
   method: 'DELETE'
 });
 
@@ -29821,14 +29826,19 @@ api.cat.prototype.threadPool = ca({
  * @param {String, String[], Boolean} params.scrollId - A comma-separated list of scroll IDs to clear
  */
 api.clearScroll = ca({
-  url: {
-    fmt: '/_search/scroll/<%=scrollId%>',
-    req: {
-      scrollId: {
-        type: 'list'
+  urls: [
+    {
+      fmt: '/_search/scroll/<%=scrollId%>',
+      req: {
+        scrollId: {
+          type: 'list'
+        }
       }
+    },
+    {
+      fmt: '/_search/scroll'
     }
-  },
+  ],
   method: 'DELETE'
 });
 
