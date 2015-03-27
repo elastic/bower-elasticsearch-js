a15290e7f6f4b11c9c8aa31f7c63969e8ac81532
Spencer Alger <email@spalger.com>
2015-03-26 17:22:20 -0700

    version 4.0.1

--- elasticsearch.angular.js
+++ elasticsearch.angular.js
@@ -1,4 +1,4 @@
-/*! elasticsearch - v4.0.0 - 2015-03-26
+/*! elasticsearch - v4.0.1 - 2015-03-26
  * http://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/index.html
  * Copyright (c) 2015 Elasticsearch BV; Licensed Apache 2.0 */
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
